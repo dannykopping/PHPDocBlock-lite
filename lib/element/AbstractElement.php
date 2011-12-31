147ecaea476dc044d118727743c3514520423f74
Danny Kopping <dannykopping@gmail.com>
2011-12-31 21:23:19 +0200

    Formatting fix (tabs converted to spaces)

--- lib/element/AbstractElement.php
+++ lib/element/AbstractElement.php
@@ -1,26 +1,27 @@
 <?php
-	abstract class AbstractElement
-	{
-		protected $reflectionObject;
+    abstract class AbstractElement
+    {
+        protected $reflectionObject;
 
-		/**
-		 * Store a reference to this Element's related Reflection instance
-		 *
-		 * @param Reflector $reflectionObject
-		 */
-		public function setReflectionObject(Reflector $reflectionObject)
-		{
-			$this->reflectionObject = $reflectionObject;
-		}
+        /**
+         * Store a reference to this Element's related Reflection instance
+         *
+         * @param Reflector $reflectionObject
+         */
+        public function setReflectionObject(Reflector $reflectionObject)
+        {
+            $this->reflectionObject = $reflectionObject;
+        }
 
-		/**
-		 * Get a reference to this Element's related Reflection instance
-		 *
-		 * @return mixed
-		 */
-		public function getReflectionObject()
-		{
-			return $this->reflectionObject;
-		}
-	}
+        /**
+         * Get a reference to this Element's related Reflection instance
+         *
+         * @return mixed
+         */
+        public function getReflectionObject()
+        {
+            return $this->reflectionObject;
+        }
+    }
+
 ?>
